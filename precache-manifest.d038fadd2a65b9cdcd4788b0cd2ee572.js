9ea5168f7b26792b8d777ad992b5ee98aea5961c
Selvakumar2002 <selvavenkat50@gmail.com>
2024-11-05 12:19:52 +0530

    first commit

---
+++ precache-manifest.d038fadd2a65b9cdcd4788b0cd2ee572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e56c50798c67c7f874e08ce5a12d239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a454c97692bf60c00615",
+    "url": "/static/css/main.443bc090.chunk.css"
+  },
+  {
+    "revision": "3a60f7b2b0ab42cb9a27",
+    "url": "/static/js/2.94ebf5dc.chunk.js"
+  },
+  {
+    "revision": "e9574eec87493a07c4fe",
+    "url": "/static/js/3.ecfed063.chunk.js"
+  },
+  {
+    "revision": "a454c97692bf60c00615",
+    "url": "/static/js/main.086f73d3.chunk.js"
+  },
+  {
+    "revision": "fecb87012e8bdb946948",
+    "url": "/static/js/runtime~main.fb516f30.js"
+  }
+]);
